0df3d0e6315261c08d5b55fc56a6e7672425b4c0
LucasBrunner <lucasbrunner@gmail.com>
2023-01-17 16:07:30 -0500

    Clarity fixes

--- Fibonacchi.java
+++ Fibonacchi.java
@@ -3,19 +3,19 @@
  */
 public class Fibonacchi {
 
-  public static int fibonacchi(int depth, int n1, int n2) {
-    if (depth <= 1) {
-      return n1 + n2;
+  public static int fibonacchi(int sequenceNum, int n1, int n2) {
+    if (sequenceNum > 0) {
+      return fibonacchi(sequenceNum - 1, n2, n1 + n2);
     }
-    return fibonacchi(depth - 1, n2, n1 + n2);
+    return n1;
   }
 
   public static int fibonacchi(int sequenceNum) {
-    return fibonacchi(sequenceNum - 1, 0, 1);
+    return fibonacchi(sequenceNum, 0, 1);
   }
 
-  public static String ordinalIndicated(int number) {
-    String numberString = ((Integer)number).toString();
+  public static String ordinalIndicated(Integer number) {
+    String numberString = number.toString();
 
     String ordinalIndicator = "th";
     if (numberString.length() <= 1 || numberString.charAt(numberString.length() - 2) != '1' || numberString.charAt(numberString.length() - 2) == '-') {
